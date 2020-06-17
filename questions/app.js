ac977ebc061a57afe47d207075de4d28ba4a97d7
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-06-17 14:46:50 -0400

    Finished questions project

--- questions/app.js
+++ questions/app.js
@@ -1,11 +1,24 @@
 //using selectors inside the element
+const questions = Array.from(document.querySelectorAll('.question'));
+
+questions.forEach((question) => {
+  const btn = question.querySelector('.question-btn');
+  btn.addEventListener('click', () => {
+    questions.forEach((item) => {
+      if (item !== question) {
+        item.classList.remove('show-text');
+      }
+    });
+    question.classList.toggle('show-text');
+  });
+});
 
 // traversing the dom
-const buttons = document.querySelectorAll('.question-btn');
+// const buttons = document.querySelectorAll('.question-btn');
 
-buttons.forEach((button) =>
-  button.addEventListener('click', (e) => {
-    const question = e.currentTarget.parentElement.parentElement;
-    question.classList.toggle('show-text');
-  })
-);
+// buttons.forEach((button) =>
+//   button.addEventListener('click', (e) => {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle('show-text');
+//   })
+// );
